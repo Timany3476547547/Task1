883763b31948bb4b5818d7e496865d45a2a202f1
Timany3476547547 <timur_vins@mail.ru>
2023-06-02 16:25:11 +0300

    first commit

---
+++ OOP1/Person.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Person
+    {
+        // Поля
+        private string name; // доступ только из класса
+        protected int age; // доступ из класса и его наследников
+        public string gender; // доступ из любого места программы
+        internal string address; // доступ из любого места внутри того же проекта
+        protected internal string phoneNumber; // доступ из любого места внутри того же проекта или его наследников
+
+        // Константы
+        public const string DefaultName = "Unknown";
+        public readonly string Nationality = "Russian"; // поле только для чтения
+
+        // Конструкторы
+        public Person()
+        {
+            name = DefaultName;
+            age = 0;
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+            age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Свойства
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string FullName
+        {
+            get { return $"{name} ({age} years old)"; }
+        }
+
+        // Методы
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello, my name is {name}.");
+        }
+
+        public void SayAge()
+        {
+            Console.WriteLine($"I am {age} years old.");
+        }
+
+        public void SayAddress()
+        {
+            Console.WriteLine($"My address is {address}.");
+        }
+
+        public string GetPhoneNumber()
+        {
+            return phoneNumber;
+        }
+
+        public void SetPhoneNumber(string phoneNumber)
+        {
+            this.phoneNumber = phoneNumber;
+        }
+        // перегруженный метод SetPhoneNumber
+        public void SetPhoneNumber(int areaCode, string phoneNumber)
+        {
+            this.phoneNumber = $"{areaCode}-{phoneNumber}";
+        }
+
+        public static void SayHelloTo(Person person)
+        {
+            Console.WriteLine($"Hello, {person.Name}!");
+        }
+
+        // Финализатор
+        ~Person()
+        {
+            Console.WriteLine($"Person object with name {name} has been destroyed.");
+        }
+    }
+
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OOP1;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        Person person2 = new Person("John");
+        Person person3 = new Person("Jane", 30);
+
+        person1.Name = "Bob";
+        person1.Age = 25;
+        person1.Address = "123 Main St.";
+        person1.PhoneNumber = "555-1234";
+
+        person2.Address = "456 Main St.";
+        person2.SetPhoneNumber("555-5678");
+
+        person3.SetPhoneNumber(555, "9012");
+
+        person1.SayHello(); // Hello, my name is Bob.
+        person1.SayAge(); // I am 25 years old.
+        person1.SayAddress(); // My address is 123 Main St.
+        Console.WriteLine(person1.FullName); // Bob (25 years old)
+
+        person2.SayHello(); // Hello, my name is John.
+        person2.SayAge(); // I am 0 years old.
+
+        Person.SayHelloTo(person3); // Hello, Jane!
+
+        Console.ReadKey();
+    }
+}
